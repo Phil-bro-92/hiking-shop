3b7837c670789efc690b16da4722abe83c76993d
Phil Broadley <philbrocoding@hotmail.com>
2023-06-09 17:43:30 +0100

    Basic file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Containers/Home";
+import Detail from "./Containers/Detail";
+import WishList from "./Containers/Detail";
+import Basket from "./Containers/Basket";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Detail" element={<Detail />} />
+        <Route exact path="/WishList" element={<WishList />} />
+        <Route exact path="/Basket" element={<Basket />} />
+      </Routes>
+    </Router>
   );
 }
 
